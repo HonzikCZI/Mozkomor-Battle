a63b8ddf15d538c4f5f366d81539c18a764e3008
HonzikCZI <jan.sara.cze@gmail.com>
2025-07-20 10:27:18 +0200

    Add game, player, and mozkomor classes with initial methods

--- main.py
+++ main.py
@@ -14,6 +14,65 @@ pygame.display.set_caption("MOZKOMOR BATTLE")
 fps = 60
 clock = pygame.time.Clock()
 
+# classy
+class game:
+    def __init__(self):
+        pass
+
+    # kód který je volán stale dokola
+    def update(self):
+        pass
+
+    # vykresluje vše ve hře
+    def draw(self):
+        pass
+
+    # kontroluje kolizi
+    def check_collision(self):
+        pass
+
+    # zahají nové kolo
+    def start_new_round(self):
+        pass
+
+    # vybírá nového mozkomora
+    def choose_new_target(self):
+        pass
+
+    # pozastavení hry
+    def pause_game(self):
+        pass
+
+    # resetuje hru
+    def reset_game(self):
+        pass
+
+
+class player(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+    # kód který se volá stále dokola
+    def updates(self):
+        pass
+
+    # návrat do bezpečné zóny
+    def back_to_safe_zone(self):
+        pass
+
+    # vrací hráče do výhozí pozice
+    def reset(self):
+        pass
+
+
+class mozkomor(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+    # kód který je volán stále dokola
+    def update(self):
+        pass
+
 
 ###################HLAVNÍ CYKLUS#########################
 lets_continue = True
